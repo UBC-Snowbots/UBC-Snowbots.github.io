d14cca4ae2ea3c8d643b33f56b2adc83da3b0e89
Simon Jinaphant <SimonJinaphant@users.noreply.github.com>
2015-11-20 21:27:59 -0800

    Uploaded parser

---
+++ misc/generator.py
@@ -0,0 +1,26 @@
+from sys import argv
+from re import sub
+
+templateString = ""
+with open(argv[1], "r") as template:
+	templateString = template.read()
+
+result = []
+with open(argv[2], "r") as input:
+
+	for line in input.readlines():
+		cell = line.split(",")
+
+		copyString = templateString
+		team = cell[0]
+		first = cell[2]
+		last = cell[3].strip()		#Get rid of \r and \n
+
+		copyString = copyString.replace("$FIRST", first)
+		copyString = copyString.replace("$LAST", last)
+		copyString = copyString.replace("$TEAM", team)
+
+		result.append(copyString+"\n\n")
+
+with open(argv[2][:-4]+".txt", "w") as output:
+	output.writelines(result)
