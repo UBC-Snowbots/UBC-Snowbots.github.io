245b7b7453091bc5d20951d57002647e0e2f19dd
SimonJinaphant <SimonJinaphant@users.noreply.github.com>
2016-01-31 10:39:34 -0800

    Updated roster to be alphabet order by last name

--- misc/generator.py
+++ misc/generator.py
@@ -14,13 +14,16 @@ with open(argv[2], "r") as input:
 
 		copyString = templateString
 		team = cell[0]
-		first, last = cell[1].split()
-		last.strip()
+		first = cell[2]
+		last = cell[3].strip()		#Get rid of \r and \n
 		
 		if not isfile("{0}/{1}_{2}.JPG".format(argv[3],first, last)):
 			#Insert blank photo members without a roster picture
 			copyString = copyString.replace("$FIRST_$LAST", "Blank")
 		
+		if "Lead" in team:
+			copyString = copyString.replace("<li>", "<li class=\"teamlead\">")
+
 		copyString = copyString.replace("$FIRST", first)
 		copyString = copyString.replace("$LAST", last)
 		copyString = copyString.replace("$TEAM", team)
